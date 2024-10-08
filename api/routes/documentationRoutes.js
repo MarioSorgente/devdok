69eb17c159dd503251ee1e66d6dd58709fd21409
MarioSorgente <94972242+MarioSorgente@users.noreply.github.com>
2024-10-08 21:37:57 +0200

    Update documentationRoutes.js

--- api/routes/documentationRoutes.js
+++ api/routes/documentationRoutes.js
@@ -1,1 +1,10 @@
+// routes/documentationRoutes.js
+
+const express = require('express');
+const router = express.Router();
+const documentationController = require('../controllers/documentationController');
+
+router.post('/generate-doc', documentationController.generateDocumentation);
+
+module.exports = router;
 
