826d984d3d4b9a8b016e10fb073e66e8884c1e52
MarioSorgente <94972242+MarioSorgente@users.noreply.github.com>
2024-10-08 21:39:02 +0200

    Update app.js

--- api/app.js
+++ api/app.js
@@ -1,1 +1,17 @@
+// app.js
+
+const express = require('express');
+const app = express();
+const documentationRoutes = require('./routes/documentationRoutes');
+const errorHandler = require('./utils/errorHandler');
+
+app.use(express.json());
+
+// Routes
+app.use('/api', documentationRoutes);
+
+// Error Handling Middleware
+app.use(errorHandler);
+
+module.exports = app;
 
